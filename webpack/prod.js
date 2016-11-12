4c54b7795bd22f10a3134e4f2f8e9795bc8254e4
mthpvg <mth.pvg@gmail.com>
2016-11-12 14:44:15 +0100

    feat: uglify on prod

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,1 +1,12 @@
-module.exports = {};
+var webpack = require('webpack')
+
+module.exports = {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+      , sourceMap: false
+    })
+  ]
+};
